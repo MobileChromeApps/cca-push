f53dc2337107b4de1c29026abc344a9cc796e3f7
Andrew Grieve <agrieve@chromium.org>
2014-05-15 14:23:03 -0400

    Add --info command. Add support for push without form-data.

--- src/push.js
+++ src/push.js
@@ -70,7 +70,6 @@ function zipDir(zip, dir) {
   });
 }
 
-
 // Takes a Node-style callback: function(err).
 exports.push = function(target, dir, cb) {
   // Fetch the manifest so we can get the app's name for use in the push.
@@ -110,41 +109,69 @@ exports.push = function(target, dir, cb) {
   }
   zipContents.copy(crxContents, 16);
 
-  var req = doPost(target, 'push', { type: 'crx', name: appName }, cb);
-  req.form().append("file", crxContents, { filename: 'push.crx', contentType: 'application/octet-stream' });
+  exports.info(target, function(e, res) {
+    var req;
+    if (e && e.statusCode == 404) {
+      // Old-style HarnessPush.
+      req = doRequest('POST', target, 'push', { type: 'crx', name: appName }, cb);
+      req.form().append('file', crxContents, { filename: 'push.crx', contentType: 'application/octet-stream' });
+    } else if (!e) {
+      // New-style.
+      req = doRequest('POST', target, 'push', { type: 'crx', name: appName }, cb, crxContents.length);
+      req.write(crxContents);
+    }
+  });
 }
 
-function doPost(target, action, queryParams, cb) {
+function doRequest(method, target, action, queryParams, cb, contentLength) {
+  var targetParts = target.split(':');
+  var host = targetParts[0];
+  var port = +(targetParts[1] || 2424);
+
   // Send the HTTP request. crxContents is a Node Buffer, which is the payload.
   // Prepare the form data for upload.
   var uri = url.format({
     protocol: 'http',
-    hostname: target,
-    port: 2424,
+    hostname: host,
+    port: port,
     pathname: '/' + action,
     query: queryParams
   });
 
+  var headers = {};
+  if (typeof contentLength != 'undefined') {
+      headers['Content-Length'] = String(contentLength);
+  }
   var req = request.post({
     uri: uri,
-    method: 'POST'
-  }, function(err, res, body) {
-    if (err) {
-      cb(err);
-    } else {
-      console.log(body);
-      cb(null);
-    }
-  });
+    method: method,
+    headers: headers
+  }, cb);
   return req;
 };
 
+exports.info = function(target, cb) {
+  function afterReq(err, res, body) {
+    if (err) {
+      err.statusCode = res.statusCode;
+      cb(err);
+    } else {
+      try {
+        cb(null, JSON.parse(body));
+      } catch (e) {
+        cb(e);
+      }
+    }
+  }
+  doRequest('GET', target, 'info', {}, afterReq);
+};
+
 exports.menu = function(target, cb) {
-  doPost(target, 'menu', null, cb);
+  doRequest('POST', target, 'menu', null, cb);
 };
 
 exports.eval = function(target, someJs, cb) {
-  doPost(target, 'exec', { code: someJs }, cb);
+  doRequest('POST', target, 'exec', { code: someJs }, cb);
 };
 
 function parseArgs(argv) {
@@ -152,25 +179,27 @@ function parseArgs(argv) {
       'path': path,
       'help': Boolean,
       'menu': Boolean,
+      'info': Boolean,
       'eval': String,
       'target': String
     };
     var ret = nopt(opts, null, argv);
     if (!ret.target) {
-      ret.target = 'localhost';
+      ret.target = 'localhost:2424';
     }
-    if (!ret.path && !ret.menu && !ret.eval) {
+    if (+!!ret.path + +!!ret.menu + +!!ret.eval + +!!ret.info !== 1) {
       usage();
     }
     return ret;
 }
 
 function usage() {
-  console.log('Usage: cca-push --path=path/to/chrome_app --target=IP_ADDRESS');
+  console.log('Usage: cca-push --path=path/to/chrome_app --target=IP_ADDRESS:PORT');
   console.log('Usage: cca-push --menu');
   console.log('Usage: cca-push --eval "alert(1)"');
+  console.log('Usage: cca-push --info');
   console.log();
-  console.log('--target defaults to localhost');
+  console.log('--target defaults to localhost:2424');
   console.log('To deploy to Android over USB, use: adb forward tcp:2424 tcp:2424');
   process.exit(1);
 }
@@ -178,8 +207,15 @@ function usage() {
 function main() {
   var args = parseArgs(process.argv);
 
-  function cb(err) {
-    if (err) console.error(err);
+  function cb(err, res, body) {
+    if (err) {
+      console.error(err);
+    } else if (arguments.length == 2) {
+      // info command.
+      console.log(JSON.stringify(res, null, 4));
+    } else if (body) {
+      console.log(body);
+    }
   }
   if (args.path) {
     exports.push(args.target, args.path, cb);
@@ -187,6 +223,8 @@ function main() {
     exports.menu(args.target, cb);
   } else if (args.eval) {
     exports.eval(args.target, args.eval, cb);
+  } else if (args.info) {
+    exports.info(args.target, cb);
   }
 }
 
